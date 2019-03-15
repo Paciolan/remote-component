788f9f9a23681a4088ed93613ba95dd1a9214f50
Joel Thoms <jthoms@paciolan.com>
2019-03-15 11:18:30 -0700

    chore: 🔨 add git-cz
    replace commitizen with git-cz to handle commit message formats

---
+++ changelog.config.js
@@ -0,0 +1,77 @@
+module.exports = {
+  list: [
+    "test",
+    "feat",
+    "fix",
+    "chore",
+    "docs",
+    "refactor",
+    "style",
+    "ci",
+    "perf"
+  ],
+  maxMessageLength: 64,
+  minMessageLength: 3,
+  questions: [
+    "type",
+    "scope",
+    "subject",
+    "body",
+    "breaking",
+    "issues",
+    "lerna"
+  ],
+  scopes: [],
+  types: {
+    chore: {
+      description: "Build process or auxiliary tool changes",
+      emoji: "🛠️",
+      value: "chore"
+    },
+    ci: {
+      description: "CI related changes",
+      emoji: "🤖",
+      value: "ci"
+    },
+    docs: {
+      description: "Documentation only changes",
+      emoji: "📚",
+      value: "docs"
+    },
+    feat: {
+      description: "A new feature",
+      emoji: "✨",
+      value: "feat"
+    },
+    fix: {
+      description: "A bug fix",
+      emoji: "🐛",
+      value: "fix"
+    },
+    perf: {
+      description: "A code change that improves performance",
+      emoji: "⚡️",
+      value: "perf"
+    },
+    refactor: {
+      description: "A code change that neither fixes a bug or adds a feature",
+      emoji: "💡",
+      value: "refactor"
+    },
+    release: {
+      description: "Create a release commit",
+      emoji: "🚀",
+      value: "release"
+    },
+    style: {
+      description: "Markup, white-space, formatting, missing semi-colons...",
+      emoji: "🎨",
+      value: "style"
+    },
+    test: {
+      description: "Adding missing tests",
+      emoji: "🚨",
+      value: "test"
+    }
+  }
+};
