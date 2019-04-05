098f4ea016e38513ee66642c99398f7ad2bf9e8e
Joel Thoms <jthoms@paciolan.com>
2019-04-04 17:13:16 -0700

    chore: 🛠️ enforce 100% code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   collectCoverageFrom: ["./src/**/*.js"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 };
