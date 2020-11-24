5896d603e8ec84c8f6e563e8c730eead02d92ff1
Joel Thoms <jthoms@paciolan.com>
2020-11-23 17:31:28 -0800

    feat: ✨ soft launch getServerSideProps for Next.js apps

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { createRequires } from "./lib/createRequires";
 export { getDependencies } from "./lib/getDependencies";
 export { createRemoteComponent } from "./lib/createRemoteComponent";
 export { RemoteComponent } from "./components/RemoteComponent";
+export { getServerSideProps } from "./next";
